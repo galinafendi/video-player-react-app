21051f3eb95d0ef90c34ae305f45d1710c558451
galinafendi <galina.h.krem@gmail.com>
2021-02-12 11:25:24 -0600

    completed project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
-import App from './components/app';
-import reducers from './reducers';
+//YouTube API
+const API_KEY = 'AIzaSyBa8JwosrMuFcrOh8M6alKlpCK8tOmAzl0';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component and this component should produce some html
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+
+// take this component's generated html and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
